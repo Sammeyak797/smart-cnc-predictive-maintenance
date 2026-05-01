b19231d59fff049c7c924d7f8957434c414b0fdb
sammeyak <sammeyakwankhade@gmail.com>
2026-05-01 23:28:27 +0530

    fix(test_predict.py): added extact path for the module in the file

--- backend/test_predict.py
+++ backend/test_predict.py
@@ -1,4 +1,8 @@
 from model.predict import predict_failure
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 sample_data = {
     "type": "L",
